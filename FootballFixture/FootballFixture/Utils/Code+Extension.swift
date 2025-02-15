6fdc6f170fd8342c476c85e4cfd0374b24f4d39b
yeniObabatunde <babatundeabike3@gmail.com>
2025-02-15 23:25:45 +0100

    Create Code+Extension.swift

---
+++ FootballFixture/FootballFixture/Utils/Code+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Code+Extension.swift
+//  FootballFixture
+//
+//  Created by Sharon Omoyeni Babatunde on 14/02/2025.
+//
+
+import Foundation
+
+typealias Parameters = [String: Any]
+typealias EmptyHandler = (() -> Void)
+typealias onCompletion = ((Any) -> Void)?
